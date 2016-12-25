57ac4439874d57d01a020e99a13db6f0bde867a4
ShiyangChen <411030065@qq.com>
2016-12-25 19:31:58 +0800

    Add solution for chapter 17 test 24

---
+++ test17_24.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <regex>
+
+using namespace std;
+
+int main() {
+    string phone = "(\\()?(\\d{3})(\\))?([-. ])?(\\d{3})([-. ])?(\\d{4})";
+    regex r(phone);
+    string s;
+    string fmt = "$2.$5.$7";
+    while(getline(cin, s)) {
+        cout << regex_replace(s, r, fmt) << endl;
+    }
+    return 0;
+}
