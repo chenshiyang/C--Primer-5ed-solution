fdce8032ae15b1de0d211f49458da304a99ce023
ShiyangChen <411030065@qq.com>
2016-12-27 15:57:23 +0800

    Add solution for chapter 17 test 37, test 38

---
+++ test17_37_38.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <cstddef>
+#include <cstring>
+using namespace std;
+
+int main() {
+    string path("test17_37.txt");
+    ifstream is(path);
+	char sink[80] = {'\0'};
+	size_t size = 80;
+	char delim = '\n';
+	while(is.getline(sink, size, delim)) {
+		cout.write(sink, size);
+		cout << "\n";
+		memset(sink, '\0', size);
+	}
+	return 0;
+}
